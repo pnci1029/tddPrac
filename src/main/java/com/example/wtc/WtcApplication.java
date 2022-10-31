a32c9b97c7180c6de2f244df17604a76570778fd
ChHong <pnci1029@gmail.com>
2022-10-31 15:59:18 +0900

    Initial commit

---
+++ src/main/java/com/example/wtc/WtcApplication.java
@@ -0,0 +1,13 @@
+package com.example.wtc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WtcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WtcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/wtc/WtcApplicationTests.java
@@ -0,0 +1,20 @@
+package com.example.wtc;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WtcApplicationTests {
+
+	/**
+	 * 비밀번호 생성 테스트코드 작성
+	 */
+
+	@DisplayName("널값 출력")
+	@Test
+	void NullPoint() {
+		
+	}
+
+}
